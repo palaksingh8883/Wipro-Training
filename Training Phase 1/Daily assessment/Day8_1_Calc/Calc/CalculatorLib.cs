4b0b57e728fd1cb4ffc39a9ea41268999900c4a7
palaksingh8883 <86922910+palaksingh8883@users.noreply.github.com>
2025-03-18 20:49:23 +0530

    Add files via upload

---
+++ Training Phase 1/Daily assessment/Day8_1_Calc/Calc/CalculatorLib.cs
@@ -0,0 +1,16 @@
+﻿//Main Logic, it does actual calculation.
+
+namespace CalculatorLib
+{
+    public class Calc
+    {
+        public double Add(double x, double y) => x + y;
+        public double Subtract(double x, double y) => x - y;
+        public double Multiply(double x, double y) => x * y;
+        public double Divide(double x, double y)
+        {
+            if (y == 0) throw new DivideByZeroException("Cannot divide by zero.");
+            return x / y;
+        }
+    }
+}
